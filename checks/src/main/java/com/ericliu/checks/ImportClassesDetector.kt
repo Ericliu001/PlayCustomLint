5457f6e3faa456f78524122f65562489382fca03
Eric Liu <eric.liu.developer@gmail.com>
2022-02-05 22:01:44 -0800

    detect classes that imported more than 3 views

--- checks/src/main/java/com/ericliu/checks/ImportClassesDetector.kt
+++ checks/src/main/java/com/ericliu/checks/ImportClassesDetector.kt
@@ -2,12 +2,17 @@ package com.ericliu.checks
 
 import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.*
+import com.intellij.psi.PsiClass
 import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UFile
 import org.jetbrains.uast.UImportStatement
 
 class ImportClassesDetector : Detector(), SourceCodeScanner {
     override fun getApplicableUastTypes(): List<Class<out UElement>>? {
-        return listOf(UImportStatement::class.java)
+        return listOf(
+            UFile::class.java,
+            UImportStatement::class.java
+        )
     }
 
     override fun createUastHandler(context: JavaContext): UElementHandler? {
@@ -15,12 +20,31 @@ class ImportClassesDetector : Detector(), SourceCodeScanner {
     }
 
     private class ImportVisitor(val context: JavaContext) : UElementHandler() {
+        private val viewImports = mutableListOf<String>()
+
+        override fun visitFile(node: UFile) {
+            viewImports.clear()
+        }
+
+
         override fun visitImportStatement(statement: UImportStatement) {
-            context.report(
-                ISSUE,
-                context.getLocation(statement),
-                "Don't import here. I don't like it!!!"
-            )
+            val resolved = statement.resolve()
+            if (resolved is PsiClass) {
+                val qualifiedName = (resolved as PsiClass).qualifiedName ?: ""
+                if (qualifiedName.contains("android.view.")) {
+                    viewImports.add(qualifiedName)
+
+                    if (viewImports.count() >= 3) {
+                        context.report(
+                            ISSUE,
+                            context.getLocation(statement),
+                            "Don't import here. I don't like it!!!"
+                        )
+                    }
+
+                }
+            }
+
         }
     }
 
